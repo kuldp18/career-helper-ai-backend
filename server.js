671bbc8fab74b2696b06b17527e17fbdf5fc0633
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-10-07 14:17:16 +0530

    connect mongodb

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 const PORT = process.env.PORT || 6000;
+
+// connect to db
+
+try {
+  await mongoose.connect(process.env.DB_URL);
+  console.log('DB Connected!');
+} catch (error) {
+  console.log(error);
+}
 
 // starting a server
 app.listen(PORT, () => {
@@ -9,5 +20,7 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.json({
+    message: 'Welcome to career helper backend!',
+  });
 });
